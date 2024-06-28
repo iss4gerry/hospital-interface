735d09983c28bb3a8f2aa0aec785078230a6c011
Gerry <150305559+iss4gerry@users.noreply.github.com>
2024-06-28 16:41:19 +0700

    update

--- view.js
+++ view.js
@@ -2,7 +2,7 @@ class HospitalView {
     static registerView(objArr) {
         console.log(`save data success {"username":${objArr[0].username},"password":${objArr[0].password},"role":${objArr[0].position}. Total employee : ${objArr[1]}`)
     }
-     
+    
     static loginView(user) {
         console.log(`user ${user[0].username} berhasil login`)
     }
